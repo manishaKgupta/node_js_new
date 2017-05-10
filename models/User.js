823da7a714bd301ebf8e81b5cc3fbd66dfd17d0e
Łukasz Szewczak <lukaszewczak@gmail.com>
2017-05-10 06:28:04 +0200

    [feat] Add unique validation to the email and username fields

--- models/User.js
+++ models/User.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
 
 const UserSchema = new mongoose.Schema({
     username: {
         type: String,
         lowercase: true,
+        unique: true,
         required: [true, "can't be blank"],
         match: [/^[a-zA-Z0-9]+$/, 'is invalid'],
         index: true
@@ -11,6 +13,7 @@ const UserSchema = new mongoose.Schema({
     email: {
         type: String,
         lowercase: true,
+        unique: true,
         required: [true, "can't be blank"],
         match: [/\S+@\S+\.\S+/, 'is invalid'],
         index: true
@@ -20,5 +23,7 @@ const UserSchema = new mongoose.Schema({
     hash: String,
     salt: String
 }, {timestamps: true});
+
+UserSchema.plugin(uniqueValidator, {message: 'is already taken.'});
 
 mongoose.model('User', UserSchema);
