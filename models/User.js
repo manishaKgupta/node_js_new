62f2d98b1f083ff5542e2461fea8eb1b61e99928
Łukasz Szewczak <lukaszewczak@gmail.com>
2017-05-10 06:54:55 +0200

    [feat] Create a method on the user model to generate a JWT

--- models/User.js
+++ models/User.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
 const crypto = require('crypto');
+const jwt = require('jsonwebtoken');
+const secret = require('../config').secret;
 
 const UserSchema = new mongoose.Schema({
     username: {
@@ -35,6 +37,18 @@ UserSchema.methods.setPassword = (password) => {
 UserSchema.methods.validPassword = (password) => {
     const hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
     return this.hash === hash;
+};
+
+UserSchema.methods.generateJWT = () => {
+    const today = new Date();
+    const exp = new Date(today);
+    exp.setDate(today.getDate() + 60);
+
+    return jwt.sign({
+        id: this._id,
+        username: this.username,
+        exp: parseInt(exp.getTime() / 1000)
+    }, secret);
 };
 
 mongoose.model('User', UserSchema);
