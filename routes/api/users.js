aec6bbe52ba7b9c5fe2af24a524de011d759a5db
Łukasz Szewczak <lukaszewczak@gmail.com>
2017-05-12 22:43:09 +0200

    [feat] Create the registration route

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,4 +4,17 @@ const passport = require('passport');
 const User = mongoose.model('User');
 const auth = require('../auth');
 
+router.post('/users', (req, res, next) => {
+    const user = new User();
+
+    user.username = req.body.user.username;
+    user.email = req.body.user.email;
+    user.setPassword(req.body.user.password);
+
+    user.save().then(() => {
+        return res.json({user: user.toAuthJSON()});
+    }).catch(next);
+
+});
+
 module.exports = router;
