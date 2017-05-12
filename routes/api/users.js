328fb18a61940e2398c2ce17b93519f59d6c5a7d
Łukasz Szewczak <lukaszewczak@gmail.com>
2017-05-12 23:08:35 +0200

    [feat] Create the login route

--- routes/api/users.js
+++ routes/api/users.js
@@ -17,4 +17,27 @@ router.post('/users', (req, res, next) => {
 
 });
 
+router.post('/users/login', (req, res, next) => {
+    if (!req.body.user.email) {
+        return res.status(422).json({errors: {email: "can't be blank"}});
+    }
+
+    if (!req.body.user.password) {
+        return res.status(422).json({errors: {password: "can't be blank"}});
+    }
+
+    passport.authenticate('local', {session: false}, (err, user, info) => {
+        if (err) {
+            return next(err);
+        }
+
+        if (user) {
+            user.token = user.generateJWT();
+            return res.json({user: user.toAuthJSON()});
+        } else {
+            return res.status(422).json(info);
+        }
+    })(req, res, next);
+});
+
 module.exports = router;
