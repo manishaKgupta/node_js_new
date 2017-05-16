5490475a5d2847178b0fb4d4bc60c515d3914d4e
Łukasz Szewczak <lukaszewczak@gmail.com>
2017-05-16 11:06:59 +0200

    [feat] Prepopulate req.profile with the user's data when :username path exists

--- routes/api/profiles.js
+++ routes/api/profiles.js
@@ -3,4 +3,16 @@ const mongoose = require('mongoose');
 const User = mongoose.models('User');
 const auth = require('../auth');
 
+router.param('username', (req, res, next, username) =>{
+    User.findOne({username: username}).then((user) => {
+        if (!user) {
+            return res.sendStatus(404);
+        }
+
+        req.profile = user;
+
+        return next();
+    }).catch(next);
+});
+
 module.exports = router;
