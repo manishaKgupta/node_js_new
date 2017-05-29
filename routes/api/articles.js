e51cff816f9cefd35b40d708dbb94e747904d456
Łukasz Szewczak <lukaszewczak@gmail.com>
2017-05-29 22:23:14 +0200

    [feat] Add endpoint for creating articles

--- routes/api/articles.js
+++ routes/api/articles.js
@@ -7,4 +7,20 @@ const Article = mongoose.model('Article');
 const User = mongoose.model('User');
 const auth = require('../auth');
 
+router.post('/', auth.required, (req, res, next) => {
+    User.findById(req.payload.id).then(user => {
+        if (!user) {
+            return res.sendStatus(401);
+        }
+
+        const article = new Article(req.body.article);
+        article.author = user;
+
+        return article.save().then(_ => {
+            console.log(article.author);
+            return res.json({article: article.toJSONFor(user)});
+        });
+    }).catch(next);
+});
+
 module.exports = router;
